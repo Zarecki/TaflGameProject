2c0aedc287820432a29c944b6799f6af1d852279
Zarecki <jonzarecki@gmail.com>
2021-06-07 11:15:45 +0100

    add conditional component rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import {useState} from "react";
 import './App.css';
 import Game from './containers/Game'
 import Rules from './components/Rules'
 
 function App() {
+
+  function beginGameToggle(){
+    console.log("Triggered")
+    beginGame? setBeginGame(false) : setBeginGame(true);
+  };
+
+  const [beginGame, setBeginGame]= useState(false);
+
+  if (beginGame){
+    return (
+      <>
+        <h1>Hello, World!</h1>
+        <Game/>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Hello, World!</h1>
-      <Game/>
       <Rules/>
+      <button onClick={beginGameToggle}>ToggleBeginGame</button>
     </>
   );
 }
